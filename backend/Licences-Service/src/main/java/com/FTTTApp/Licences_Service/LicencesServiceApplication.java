78fbff247dd928c3c406099ea46630d4ed59167d
Balkis1234 <saidibalkis37@gmail.com>
2026-03-02 21:46:56 +0100

    crud integration

---
+++ backend/Licences-Service/src/main/java/com/FTTTApp/Licences_Service/LicencesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.FTTTApp.Licences_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicencesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicencesServiceApplication.class, args);
+	}
+
+}
